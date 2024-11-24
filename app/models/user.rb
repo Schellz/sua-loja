e95d1ae5b7ded961b669ed875aac547ef2fd5556
Schellz <diegoschell86@gmail.com>
2024-11-24 19:14:01 -0300

    criado os modelos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
-end
+    has_secure_password
+  
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :role, presence: true
+  end
+  
